24cec34594e916d495161a6e22bd61655f748c95
Pedro França <pfranca.neto@gmail.com>
2025-06-15 23:06:58 -0300

    config shadcn + outras configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import './global.css';
+import { Button } from './components/ui/button';
+
 export function App() {
-  return <h1>hello world</h1>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Enviar</Button>
+    </div>
+  );
 }
