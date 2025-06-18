8f45adf1c92c49591783129aed3b2d5e1447507a
Pedro França <pfranca.neto@gmail.com>
2025-06-17 21:43:56 -0300

    uninstall react helmet (conflited)

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,7 @@ import './global.css';
 
 import { RouterProvider } from 'react-router';
 import { router } from './router';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 export function App() {
-  return (
-    <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <RouterProvider router={router} />
-    </HelmetProvider>
-  );
+  return <RouterProvider router={router} />;
 }
