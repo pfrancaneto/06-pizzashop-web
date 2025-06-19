90788cb8d716db39a8cfd08d769365c5ad7c4907
Pedro França <pfranca.neto@gmail.com>
2025-06-19 14:38:12 -0300

    adicionando HeadProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import './global.css';
 import { RouterProvider } from 'react-router';
 import { router } from './router';
 import { Toaster } from 'sonner';
+import { HeadProvider } from 'react-head';
 
 export function App() {
   return (
-    <>
+    <HeadProvider>
       <Toaster richColors />
       <RouterProvider router={router} />
-    </>
+    </HeadProvider>
   );
 }
