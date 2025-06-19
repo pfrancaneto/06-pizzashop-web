b9a5a8a7605a31fd605f4ddc8537d9c9516dfb08
Pedro França <pfranca.neto@gmail.com>
2025-06-19 12:00:43 -0300

    adicionando botão com Link

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -3,6 +3,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router';
 import { toast } from 'sonner';
 import { z } from 'zod';
 
@@ -34,6 +35,10 @@ export function SignIn() {
   return (
     <>
       <div className="p-8">
+        <Button variant="ghost" asChild className="absolute top-8 right-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
